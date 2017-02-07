8fc8a54c1e7436b21326a40ae5e4aab872dc97cc
Jonathan Gerbscheid <jonathan-gerb@hotmail.com>
2017-02-07 15:09:16 +0100

    test file to get RSS for different newspapers, also style example.

---
+++ test.py
@@ -0,0 +1,34 @@
+# -*-coding:utf-8-*-
+"""Test file for the media understanding 2017 project.
+
+File name: test.py
+Author: Media Undertanding 2017
+Date created: 7/2/2017
+Date last modified: 7/2/2017
+Python Version: 3.4
+"""
+
+import feedparser
+
+newspapers = {
+    'nu': 'http://www.nu.nl/rss/Algemeen',
+    'volkskrant': 'http://www.volkskrant.nl/nieuws-voorpagina/rss.xml',
+    'telegraaf': 'http://www.telegraaf.nl/rss/'
+}
+
+
+def main():
+    """Main."""
+    d = getFeed('volkskrant')
+    for entry in d.entries:
+        print(entry.title)
+
+
+def getFeed(newspaper):
+    """Get RSS feed from given newspaper."""
+    d = feedparser.parse(newspaper[newspaper])
+    return d
+
+
+if __name__ == '__main__':
+    main()
