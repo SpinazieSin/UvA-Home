f3f2848671b6c7b28cb43361fada2ab18326ea51
SpinazieSin <thomas--g@hotmail.com>
2017-09-29 16:25:02 +0200

    updating open_day

--- open_day_uva/roster_presenter.py
+++ open_day_uva/roster_presenter.py
@@ -1,4 +1,15 @@
 
-class SchedulePresenter:
-    def __init__(self, ALProxy):
-        
+from interaction.hearing import Hearing
+from naoqi import Naoqi
+
+class RosterPresenter:
+    def __init__(self, naoqi):
+        self.naoqi = naoqi
+        self.hearing = Hearing(self.naoqi)
+
+    def present(self):
+        while True:
+            sentence = self.hearing.recognize()
+            print("Found: " + sentence)
+            break
+        print("Done")
