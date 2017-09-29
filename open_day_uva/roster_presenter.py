c63f891047761016c08353e511a828e53af5cf1f
Jonathan Gerbscheid <jonathan-gerb@hotmail.com>
2017-09-29 18:21:14 +0200

    forgot module name

--- open_day_uva/roster_presenter.py
+++ open_day_uva/roster_presenter.py
@@ -17,7 +17,7 @@ class RosterPresenter:
         self.speech.say("It is now {}".format(str(current_time)))
         eventlist = opendag_IR.get_events('./open_day_uva/opendagdata.csv')
         eventlist = opendag_IR.remove_duplicates(eventlist)
-        eventlist = get_events_after(eventlist, "14:00")
+        eventlist = opendag_IR.get_events_after(eventlist, "14:00")
         eventlist = opendag_IR.get_next_event(eventlist)
         EVENT = eventlist[0][0]
         TYPE = eventlist[0][4]
