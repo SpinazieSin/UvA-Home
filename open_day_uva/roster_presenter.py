5e67a73cb08165bce415131555f21c4713321b0c
SpinazieSin <thomas--g@hotmail.com>
2017-10-02 17:10:39 +0200

    updating roster presenter

--- open_day_uva/roster_presenter.py
+++ open_day_uva/roster_presenter.py
@@ -1,20 +1,33 @@
 
 from interaction.hearing import Hearing
 from interaction.speech import Speech
-import opendag_IR
+from opendag_IR import OpendagIR
+from language_processing import LanguageProcessing
 from naoqi import Naoqi
+import datetime
 
 class RosterPresenter:
     def __init__(self, naoqi):
         self.naoqi = naoqi
         self.hearing = Hearing(self.naoqi)
         self.speech = Speech(self.naoqi)
+        self.ir = OpendagIR()
+        self.nlp = LanguageProcessing()
 
     def present(self):
         self.speech.say("Hi everyone!")
-        # the next event is
+
+        current_time = datetime.datetime.now().strftime('%H:%M')
+        self.speech.say("It is now {}".format(str(current_time)))
+        event_list = self.ir.get_events_after("14:00")
+        next_event = self.ir.get_next_event(event_list)
+        [EVENT, TYPE, TIME] = [next_event[0], next_event[4], next_event[1]]
+        sentence = "The next event is a {}, called {}, at {}".format(TYPE, EVENT, TIME)
+        self.speech.say(sentence)
+
         while True:
-            sentence = self.hearing.recognize()
+            sentence = self.hearing.recognize().lower()
             print("Found: " + sentence)
+            command = self.nlp.get_command(sentence)
             break
         print("Done")
